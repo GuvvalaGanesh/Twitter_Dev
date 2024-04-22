1dbf4855a1cf7c52fe8d926a6dc6c92cd5f2765a
GuvvalaGanesh <ganeshguvvala00@gmail.com>
2024-04-22 08:22:04 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const Tweet = require('./models/tweet');
+// const HashtagRepository = require('./repository/hashtag-repository');
+const TweetService = require('./services/tweet-service');
 
 app.listen(3000, async() => {
 
     console.log('Server started');
     await connect();
     console.log('Mongodb connected');
-    const tweets = await Tweet.find({
-        content: ['My first tweet','Third tweet']
-    })
-    console.log(tweets);
+
+    /*const repo = new HashtagRepository();
+    let response = await repo.findByName(['Trend','Java']);
+    console.log(response);
+    response = response.map(tags => tags.title);
+    console.log(response);*/
+
+    const repo = new TweetService();
+    const tweet = await repo.create({
+        content: 'This is after #processing really #Excited, Lets #start doing #coding'
+    });
+    console.log(tweet);
 });
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,19 +1,34 @@
-const { TweetRepository } = require('../repository/index');
+const { TweetRepository, hashtagRepository, HashtagRepository } = require('../repository/index');
 
 class TweetService {
     constructor() {
         this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
 
     async create(data) {
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g); // this extracts hashtags
-        tags = tags.map((tag) => tag.substring(1));
-        console.log(tags);
+        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1)); // this extracts hashtags
+        // console.log(tags);
         const tweet = await this.tweetRepository.create(data);
+        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        // console.log(alreadyPresentTags);
+        let titleOfPresentTags = alreadyPresentTags.map(tags => tags.title);
+        let newTags = tags.filter(tag => !titleOfPresentTags.includes(tag));
+        newTags = newTags.map(tag => {
+            return {title: tag, tweets: [tweet.id]}
+        });
+        await this.hashtagRepository.bulkCreate(newTags);
+        alreadyPresentTags.forEach((tag) => {
+            tag.tweets.push(tweet.id);
+            tag.save();
+        })
+        // console.log(response);
+
+        // [excited, coding, js, virat] --> [excited, virat]
         // todo create hashtags and add here
         /**
-         * 1. bulkcreate in mongoose
+         * 1. bulkcreate in mongoose is done by "insertMany" function
          * 2. Filter title of hashtag based on multiple tags
          * 3. How to add tweet_id inside all the hashtags
          */
